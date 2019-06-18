4d29dc1e1de13b6ba7526d903532b7d19697dec8
Greg Stephen <GregAStephen@gmail.com>
2019-06-17 21:04:25 -0500

    got them goats

--- src/components/GoatCorral/GoatCorral.js
+++ src/components/GoatCorral/GoatCorral.js
@@ -1,26 +1,18 @@
 import React from 'react';
 
+import Goat from '../Goat/Goat';
 import './GoatCorral.scss';
 
 class GoatCorral extends React.Component {
   render() {
     const { goats } = this.props;
     const makeGoats = goats.map(goat => (
-      // <Goat goat={ goat } />
-      <div id={goat.id} className="col-4">
-        <div className="card">
-         <h2>{goat.name}</h2>
-         <img className="goat-image" src={goat.imageUrl} alt=''></img>
-         <div className="card-body">
-          <p>{goat.disposition}</p>
-         </div>
-        </div>
-      </div>
+      <Goat key={goat.id} goat={ goat } />
     ));
 
     return (
-      <div className="container">
-        <div className="row">
+      <div className="GoatCorral container">
+        <div className="card-columns">
         { makeGoats }
         </div>
       </div>
